72642c81dfb3cbed9401c7a6be7eaee4eb9cdb0f
Bartosz Kniaziewicz <605bart@gmail.com>
2023-04-15 18:34:02 +0200

    Added swagger

--- backend/src/main/java/pl/bartkn/ztpai/controllers/UserController.java
+++ backend/src/main/java/pl/bartkn/ztpai/controllers/UserController.java
@@ -1,6 +1,5 @@
 package pl.bartkn.ztpai.controllers;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
