ad9b606275f6578df102e240b45bf172e6c6bc5a
Bartosz Kniaziewicz <605bart@gmail.com>
2023-05-08 19:05:27 +0200

    Navbar links fix

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,23 +1,45 @@
-import React, { useState } from 'react';
+import React, {useContext, useState} from 'react';
 import {
     MDBNavbar,
     MDBContainer,
     MDBIcon,
     MDBNavbarNav,
     MDBNavbarItem,
-    MDBNavbarLink,
     MDBNavbarToggler,
     MDBNavbarBrand,
     MDBCollapse
 } from 'mdb-react-ui-kit';
 
+import AuthContext from "../context/AuthProvider";
+import {NavLink, useNavigate} from "react-router-dom";
+import {AxiosContext} from "../context/AxiosProvider";
+
+const LOGOUT_URL = '/api/logout';
+
 const NavBar = () => {
     const [showNavColor, setShowNavColor] = useState(false);
+
+    const authContext = useContext(AuthContext);
+    const axiosContext = useContext(AxiosContext);
+    const navigate = useNavigate();
+
+    const handleLogout = (e) => {
+        e.preventDefault();
+        try {
+            axiosContext.authAxios.get(LOGOUT_URL).then();
+            authContext.logout();
+            navigate('/login');
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
     return (
         <MDBNavbar expand='lg' dark bgColor='primary'>
             <MDBContainer fluid>
-                <MDBNavbarBrand href='/'>BillSplit</MDBNavbarBrand>
+                <NavLink to='/'>
+                    <MDBNavbarBrand>BillSplit</MDBNavbarBrand>
+                </NavLink>
                 <MDBNavbarToggler
                     type='button'
                     data-target='#navbarColor02'
@@ -32,19 +54,19 @@ const NavBar = () => {
                 <MDBCollapse show={showNavColor} navbar>
                     <MDBNavbarNav right fullWidth={false} className='mb-2 mb-lg-0'>
                         <MDBNavbarItem>
-                            <MDBNavbarLink href='/profile'>
+                            <NavLink to='/profile' className='nav-link'>
                                 <MDBIcon icon="user" size="3x" className='m-4'/>
-                            </MDBNavbarLink>
+                            </NavLink>
                         </MDBNavbarItem>
-                        <MDBNavbarItem>
-                            <MDBNavbarLink href='/friends'>
+                        <MDBNavbarItem className={!authContext.isAuthenticated() ? 'd-none' : ''}>
+                            <NavLink to='/friends' className='nav-link'>
                                 <MDBIcon icon="users" size="3x" className='m-4' />
-                            </MDBNavbarLink>
+                            </NavLink>
                         </MDBNavbarItem>
-                        <MDBNavbarItem>
-                            <MDBNavbarLink href='/login'>
-                                <MDBIcon icon="sign-out-alt" size="3x" className='m-4'/>
-                            </MDBNavbarLink>
+                        <MDBNavbarItem className={!authContext.isAuthenticated() ? 'd-none' : ''}>
+                            <NavLink to='/login' className='nav-link' onClick={handleLogout}>
+                                <MDBIcon icon="sign-out-alt" size="3x" className='m-4' />
+                            </NavLink>
                         </MDBNavbarItem>
                     </MDBNavbarNav>
                 </MDBCollapse>
