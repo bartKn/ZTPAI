fe65d2a3eb21a26c97a795b3baacdba62623414e
Bartosz Kniaziewicz <605bart@gmail.com>
2023-05-18 12:51:56 +0200

    Added content to user page

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -2,15 +2,32 @@ import NavBar from "./NavBar";
 import {MDBBtn, MDBCol, MDBInput, MDBRow} from 'mdb-react-ui-kit';
 import {useContext, useEffect, useState} from "react";
 import {AxiosContext} from "../context/AxiosProvider";
+import AuthContext from "../context/AuthProvider";
+import {useNavigate} from "react-router-dom";
+
+
+const USERNAME_URL = '/user/username';
+const BALANCE_URL = '/user/balance';
+const DELETE_URL = '/auth/delete';
+const POSITIVE_NUMBER_REGEX = /^(?:\d*[1-9]\d*)?$/;
 
 const Profile = () => {
 
     const axios = useContext(AxiosContext);
+    const authContext = useContext(AuthContext);
+    const navigate = useNavigate();
 
     const [userData, setUserData] = useState({
         username: '',
         balance: 0.0
     });
+
+    const[formValue, setFormValue] = useState({
+        username: '',
+        balance: ''
+    });
+
+    const[errMsg, setErrMsg] = useState('');
 
     useEffect(() => {
         axios.authAxios.get('/user/details')
@@ -20,55 +37,145 @@ const Profile = () => {
                     balance: res.data?.balance
                 })
             });
+        axios.authAxios.get('/splits')
+            .then((res) => {
+                console.log(res?.data);
+            })
     }, [axios.authAxios]);
+
+    
+    const onChange = (e) => {
+        if (e.target.name === 'balance' && !POSITIVE_NUMBER_REGEX.test(e.target.value)) {
+            e.preventDefault();
+        } else {
+            setFormValue({...formValue, [e.target.name]: e.target.value});
+        }
+    }
+
+    const handleUsernameChange = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.authAxios.patch(USERNAME_URL,
+                JSON.stringify({
+                    value: formValue.username
+                }), {
+                    headers: {'Content-Type': 'application/json'},
+                    withCredentials: true
+                }).then(response => {
+                    setUserData({
+                        username: response?.data?.username,
+                        balance: response?.data?.balance
+                    });
+                    setFormValue({...formValue, username: ''});
+                    setErrMsg('');
+            });
+        } catch (err) {
+            if (err.response.status === 409) {
+                setErrMsg(err.response.data);
+            } else {
+                setErrMsg('Error');
+            }
+        }
+    }
+
+
+    const handleBalanceChange = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.authAxios.patch(BALANCE_URL,
+                JSON.stringify({
+                    value: formValue.balance
+                }), {
+                    headers: {'Content-Type': 'application/json'},
+                    withCredentials: true
+                }).then(response => {
+                setUserData({
+                    username: response?.data?.username,
+                    balance: response?.data?.balance
+                });
+                setFormValue({...formValue, balance: ''});
+                setErrMsg('');
+            });
+        } catch (err) {
+            if (err.response.status === 409) {
+                setErrMsg(err.response.data);
+            } else {
+                setErrMsg('Error');
+            }
+        }
+    }
+
+    const handleDelete = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.authAxios.delete(DELETE_URL,
+                {withCredentials: true});
+            authContext.logout();
+            navigate('/login');
+        } catch (err) {
+            setErrMsg('Failed');
+        }
+    }
 
     return (
         <>
             <NavBar />
-            <MDBRow>
-                <MDBCol md='6'>
-
-                </MDBCol>
+            <MDBRow className='m-2'>
                 <MDBCol md='6'>
                     <div className='m-3 p-3 bg-gradient rounded-6 border border-secondary shadow-5' style={{backgroundColor : '#CCD6F6'}}>
                         <MDBRow className='m-3'>
-                            Hi {userData.username}
+                            <MDBCol md='4' className='fs-3 fw-bold'>
+                                Active splits
+                            </MDBCol>
+                        </MDBRow>
+                    </div>
+                    <div className='m-3 p-3 bg-gradient rounded-6 border border-secondary shadow-5' style={{backgroundColor : '#CCD6F6'}}>
+                        <MDBRow className='m-3'>
+                            <MDBCol md='4' className='fs-3 fw-bold'>
+                                Splits history
+                            </MDBCol>
+                        </MDBRow>
+                    </div>
+                </MDBCol>
+                <MDBCol md='6'>
+                    <div className='m-3 p-3 bg-gradient rounded-6 border border-secondary shadow-5' style={{backgroundColor : '#CCD6F6'}}>
+                        <MDBRow className='m-3 fs-3 fw-bold'>
+                            <MDBCol md='2'>
+                                Hi {userData.username}
+                            </MDBCol>
+                            <MDBCol md='6'>
+                                {errMsg}
+                            </MDBCol>
                         </MDBRow>
                         <MDBRow className='m-3'>
                             <MDBCol md='8'>
-                                <MDBInput label='Update username' id='form1' type='text' />
+                                <MDBInput label='Update username' id='form1' type='text' name='username'
+                                          value={formValue.username} onChange={onChange}/>
                             </MDBCol>
                             <MDBCol md='4'>
-                                <MDBBtn className='me-1' disabled={true}>
+                                <MDBBtn className='me-1 w-75' color='success' disabled={!formValue.username.trim().length > 0}
+                                        onClick={handleUsernameChange}>
                                     Save
                                 </MDBBtn>
                             </MDBCol>
                         </MDBRow>
                         <MDBRow className='m-3'>
-                            <MDBCol md='8'>
-                                <MDBInput label='Update email' id='form1' type='text'/>
-                            </MDBCol>
-                            <MDBCol md='4'>
-                                <MDBBtn className='me-1' color='success'>
-                                    Save
-                                </MDBBtn>
-                            </MDBCol>
-                        </MDBRow>
-                        <MDBRow className='m-3'>
-                            <MDBCol md='4'>
+                            <MDBCol md='4' className='fs-3 fw-bolder'>
                                 Balance: {userData.balance}
                             </MDBCol>
                             <MDBCol md='4'>
-                                <MDBInput label='Add balance' id='form1' type='number' step='0.1' />
+                                <MDBInput label='Add balance' id='form1' type='text'
+                                          name='balance' value={formValue.balance} onChange={onChange} />
                             </MDBCol>
                             <MDBCol md='4'>
-                                <MDBBtn className='me-1' color='success'>
+                                <MDBBtn className='me-1 w-75' color='success' disabled={!formValue.balance.length > 0}
+                                        onClick={handleBalanceChange}>
                                     Save
                                 </MDBBtn>
                             </MDBCol>
                         </MDBRow>
-                        <MDBRow className='m-3'>
-                            <MDBBtn className='me-1 w-25 text-center' color='danger'>
+                        <MDBRow className='mt-4 mb-4 ms-4'>
+                            <MDBBtn className= 'w-25 text-center' color='danger' onClick={handleDelete}>
                                 Delete account
                             </MDBBtn>
                         </MDBRow>
